1fcab25c58b25db59eec6dbc514b36e1dc1e3e39
David Papamichael <davidpapamichael@gmail.com>
2020-03-25 15:27:10 +0000

    font styling added

--- shopping-tracker/src/components/tracker.jsx
+++ shopping-tracker/src/components/tracker.jsx
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 
 class Tracker extends Component {
   state = {
-    count: 0,
-    imageURL: "https://picsum.photos/200"
+    count: 0
+  };
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold"
   };
 
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imageURL} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
