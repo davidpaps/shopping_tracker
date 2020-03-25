e8acde4f7b310b3625e7e99f36b52065638cd881
David Papamichael <davidpapamichael@gmail.com>
2020-03-25 15:39:47 +0000

    button changes colour when counter > 0

--- shopping-tracker/src/components/tracker.jsx
+++ shopping-tracker/src/components/tracker.jsx
@@ -10,14 +10,22 @@ class Tracker extends Component {
   };
 
   render() {
+    let classes = this.formatBadgeClass();
+
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={classes}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
