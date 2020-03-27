638d9017939d956d7b2a3a49e360218cfbe4edcb
David Papamichael <davidpapamichael@gmail.com>
2020-03-27 11:53:42 +0000

    Navbar made into stateless functional component

--- shopping-tracker/src/components/navbar.jsx
+++ shopping-tracker/src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="davidpaps">
-          Shopping Basket
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="davidpaps">
+        Shopping Basket
+        <span className="badge badge-pill badge-secondary m-2">
+          {this.props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
+}
 export default Navbar;
